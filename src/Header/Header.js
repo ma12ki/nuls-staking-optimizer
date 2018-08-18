3493bf751ced3d084c490208f4c8316c13fb828e
Marcin Zolkiewski <marcin.zolkiewski@scouting.org>
2018-08-18 23:17:56 +0200

    added Header

---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+  <header className="Header">
+    <h1 className="Header-title">NULS Staking Optimizer</h1>
+  </header>
+);
+
+export default Header;
