55743bee9ac9e8a282379befac4a4af10e285701
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-08-19 14:10:50 +0200

    used simulator

--- src/Optimizer/Optimizer.js
+++ src/Optimizer/Optimizer.js
@@ -2,20 +2,60 @@ import React from "react";
 
 import Inputs from "./Inputs";
 import Chart from "./Chart";
-import defaultInputValues from "./defaultInputValues";
+import simulator from "./simulator";
 import "./Optimizer.css";
 
 class Optimizer extends React.PureComponent {
   state = {
-    inputValues: defaultInputValues
+    data: [],
+    max: {}
   };
 
-  handleInputsChange = inputValues => this.setState({ inputValues });
+  handleInputsChange = ({
+    stake,
+    blockPayout,
+    blockTimeMinutes,
+    reinvestingFee,
+    days
+  }) => {
+    const data = Array(days)
+      .fill(null)
+      .map((_, i) => i + 1)
+      .map(daysBetweenReinvesting => ({
+        daysBetweenReinvesting,
+        finalStake: simulator({
+          stake,
+          blockPayout,
+          blockTimeMinutes,
+          reinvestingFee,
+          days,
+          daysBetweenReinvesting
+        })
+      }));
+
+    const max = data.reduce(
+      (highest, item) => {
+        if (item.finalStake > highest.finalStake) {
+          return item;
+        }
+        return highest;
+      },
+      { finalStake: 0 }
+    );
+
+    this.setState({ data, max });
+  };
 
   render() {
+    const { max } = this.state;
+
     return (
       <main className="Optimizer">
         <Inputs onChange={this.handleInputsChange} />
+        <div>
+          max {max.finalStake} NULS if reinvesting every{" "}
+          {max.daysBetweenReinvesting} days
+        </div>
         <Chart />
       </main>
     );
