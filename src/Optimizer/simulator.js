8bd689a245b4db3700684d086918b120f334b0a5
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-08-19 14:10:29 +0200

    implemented staking simulator

---
+++ src/Optimizer/simulator.js
@@ -0,0 +1,28 @@
+const minutesInDay = 24 * 60;
+
+const simulator = ({
+  stake,
+  blockPayout,
+  blockTimeMinutes,
+  reinvestingFee,
+  days,
+  daysBetweenReinvesting
+}) => {
+  let payoutPerDay = (minutesInDay / blockTimeMinutes) * blockPayout;
+  let dividend = 0;
+
+  for (let day = 0; day < days; day++) {
+    dividend += payoutPerDay;
+
+    if (day % daysBetweenReinvesting === 0) {
+      const oldStake = stake;
+      stake += dividend - reinvestingFee;
+      dividend = 0;
+      payoutPerDay = payoutPerDay * (stake / oldStake);
+    }
+  }
+
+  return stake + dividend;
+};
+
+export default simulator;
