3880f0d2496f015e40edea88109bdf94b138314d
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-08-19 16:41:18 +0200

    applied own material theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import indigo from "@material-ui/core/colors/indigo";
+import lightGreen from "@material-ui/core/colors/lightGreen";
 
 import Header from "./Header";
 import Optimizer from "./Optimizer";
 import "./App.css";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: lightGreen
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Optimizer />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <Optimizer />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
